0fba96180ba83dfbfb90a992b53e2f64ddad7727
Sadirov Izzatillo <sadirovizzatillo3@gmail.com>
2022-07-24 01:30:30 +0500

    Initial Commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,67 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <FormBuilder :form="form"/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import FormBuilder from './components/FormBuilder.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    name: 'App',
+    components: {
+      FormBuilder
+    },
+    data(){
+      return{
+        form:	[
+        {
+          "name": "image",
+          "field_type": "file",
+          "title": "Image",
+          "label": "Image to display",
+        },
+        {
+          "name": "full_name",
+          "field_type": "text",
+          "label": "Full Name",
+          "placeholder": "Enter your full name",
+          "required": true
+        },
+        {
+          "name":"passport_number",
+          "field_type": "password",
+          "label": "Passport Number",
+          "placeholder": "Enter your passport number",
+          "required": true
+        },
+        {
+          "name": "email",
+          "field_type": "email",
+          "label": "Email",
+          "placeholder": "Enter your email",
+          "required": true
+        },
+        {
+          "name": "phone",
+          "field_type": "tel",
+          "label": "Phone",
+          "placeholder": "Enter your phone number",
+          "required": true
+        },
+        ]
+      }
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 30px;
+  }
 </style>
---
+++ src/components/FormBuilderItem.vue
@@ -0,0 +1,66 @@
+<template>
+      <div class="shadow overflow-hidden sm:rounded-md">
+        <div class="px-4 py-5 bg-white sm:p-6">
+          <div class="col-span-6 sm:col-span-3 lg:col-span-2">
+            <label :for="name" class="flex text-sm font-medium text-gray-700">{{ label }}</label>
+            <input
+            :type="type"
+            :name="name"
+            id="postal-code"
+            :placeholder="placeholder"
+            v-model="fields"
+             required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+            >
+          </div>
+        </div>
+      </div>
+</template>
+
+<script>
+  export default {
+    name: 'FormBuilder',
+    data(){
+      return{
+        fields:[]
+      }
+    },
+    props:{
+      item:{
+        type:Object,
+        required:true
+      }
+    },
+    computed:{
+      name(){
+        return this.item.name
+      },
+      placeholder(){
+        return this.item.placeholder
+      },
+      type(){
+        return this.item.field_type
+      },
+      label(){
+        return this.item.label
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  h3 {
+    margin: 40px 0 0;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+  a {
+    color: #42b983;
+  }
+</style>
